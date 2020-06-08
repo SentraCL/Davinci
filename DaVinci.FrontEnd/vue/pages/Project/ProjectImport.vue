bb2a1d4eeae02330459e7d80075d79013e4530fb
Fernando Mateluna <fmateluna@sentra.cl>
2020-06-08 14:44:13 -0400

    Enviar nombre del proyecto al importar

--- DaVinci.FrontEnd/vue/pages/Project/ProjectImport.vue
+++ DaVinci.FrontEnd/vue/pages/Project/ProjectImport.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="container">
-    <form-wizard color="#A47B31" title="Asistente de Importación de Proyecto"
-      subtitle="Modulo de importacion de proyecto, donde se evalua y si hiciste bien tu pega.">
+    <form-wizard color="#A47B31" title="Asistente de Importación de Proyecto" subtitle="Modulo de importacion de proyecto, donde se evalua y si hiciste bien tu pega.">
       <tab-content title="Validar Proyecto" :before-change="ready">
         <div class="large-12 medium-12 small-12 cell">
           <span v-if="nameExist">Importando proyecto para: <strong>{{nameProject}}</strong></span>
-          <input v-if="step==0" accept=".dvc" type="file" id="DavinciFile" name="DavinciFile" ref="fileProject"
-            v-on:input="handleFileUpload()" hidden />
+          <input v-if="step==0" accept=".dvc" type="file" id="DavinciFile" name="DavinciFile" ref="fileProject" v-on:input="handleFileUpload()" hidden />
 
 
           <div class="row">
@@ -45,15 +43,15 @@
 
           <div class="col-md-10" style="color: right;">
             <div class="row">
-              <div class="col-4">                                
+              <div class="col-4">
                 <h4 style="overflow: hidden;text-overflow: ellipsis;"><i class="ti-blackboard"></i>&nbsp;<strong>{{importProject.project.Name}}</strong></h4>
-                <img class="avatar border-white" :src="importProject.project.Avatar64"><br/>
-                
+                <img class="avatar border-white" :src="importProject.project.Avatar64"><br />
+
               </div>
               <div class="col-sm-4">
                 <i class="ti-user"></i>&nbsp;<strong>{{importProject.project.Author}}</strong><br />
                 <i class="ti-email"></i>&nbsp;<strong>{{importProject.project.Administrator.Email}}</strong><br />
-                
+
                 <!--<input-text :label="nameProject" v-model="importProject.project.Name"></input-text><br />-->
 
                 <!-- <i class="ti-briefcase"></i><strong>{{importProject.project.Company}}</strong>-->
@@ -71,7 +69,7 @@
                 {{Object.keys(importProject.project.Epics.Types).length}}<br />
                 <i class="ti-clipboard"></i>&nbsp; Historias de usuario Cargados:
                 {{Object.keys(importProject.project.UserStories.Types).length}}<br />
-              -->                
+              -->
               </div>
               <div class="col-md-12">
 
@@ -96,47 +94,49 @@
         </span>
         <!--<button class="btn btn-success" v-if="step==1" @click="finishStep">Cargar Archivo</button>-->
       </tab-content>
-      <tab-content title="Finalisszar"  style="white-space:nowrap;">
+      <tab-content title="Finalisszar" style="white-space:nowrap;">
         <span v-if="step==1" class="row">
 
-              <div class="col-4">                                
-                <h4 style="overflow: hidden;text-overflow: ellipsis;"><i class="ti-blackboard"></i>&nbsp;<strong>{{importProject.project.Name}}</strong></h4>
-                <img class="avatar border-white" :src="importProject.project.Avatar64"><br/>
-                
-              </div>
+          <div class="col-4">
+            <h4 style="overflow: hidden;text-overflow: ellipsis;"><i class="ti-blackboard"></i>&nbsp;<strong>{{importProject.project.Name}}</strong></h4>
+            <img class="avatar border-white" :src="importProject.project.Avatar64"><br />
 
-              <div class="col-md-6" style="white-space:pre-line; padding:10px;border:4px double #A47B31; border-radius:16px 40px 0px 32px;">
-                <strong><p>{{importProject.project.Resume}}</p></strong>
-              </div>
-             
-              <div style="margin-top: 20px;" class="col-sm-4">
-                <h5><i class="ti-comment"></i>&nbsp;Este contenido sera insertado en el sistema</h5>
+          </div>
 
-                <i class="ti-user"></i>&nbsp; Total de registros de Inventos:
-                {{inventsLength}}
-                <!--<strong  v-for="repo in importProject.project.Repository">({{Object.keys(repo.WareHouse).lenght}})</strong>--><br />
+          <div class="col-md-6" style="white-space:pre-line; padding:10px;border:4px double #A47B31; border-radius:16px 40px 0px 32px;">
+            <strong>
+              <p>{{importProject.project.Resume}}</p>
+            </strong>
+          </div>
 
-                <i class="ti-book"></i>&nbsp; Epicos Cargados:
-                {{epicLength}}<br/>
-                <i class="ti-clipboard"></i>&nbsp; Historias de usuario Cargados:
-                {{userStorieLength}}<br />
-      <!-- {{Object.keys(importProject.project.Epics.Types)}}-->
-                <!--<p v-for="repo in importProject.project.Repository">
+          <div style="margin-top: 20px;" class="col-sm-4">
+            <h5><i class="ti-comment"></i>&nbsp;Este contenido sera insertado en el sistema</h5>
+
+            <i class="ti-user"></i>&nbsp; Total de registros de Inventos:
+            {{inventsLength}}
+            <!--<strong  v-for="repo in importProject.project.Repository">({{Object.keys(repo.WareHouse).lenght}})</strong>--><br />
+
+            <i class="ti-book"></i>&nbsp; Epicos Cargados:
+            {{epicLength}}<br />
+            <i class="ti-clipboard"></i>&nbsp; Historias de usuario Cargados:
+            {{userStorieLength}}<br />
+            <!-- {{Object.keys(importProject.project.Epics.Types)}}-->
+            <!--<p v-for="repo in importProject.project.Repository">
                   ({{Object.keys(repo.WareHouse).length}})
                 </p>-->
-                               
-              </div>
-             
-            
+
+          </div>
 
 
 
-            <!--<div class="col-md-12">
+
+
+          <!--<div class="col-md-12">
               <div class="form-group">
                 <span v-html="importProject.project.Resume"></span>
               </div>
             </div>-->
-          
+
         </span>
 
       </tab-content>
@@ -170,7 +170,7 @@
         importProject: {},
         invToRename: {},
         nameExist: !this.isEmptyOrSpaces(this.nameProject),
-        invents : {},
+        invents: {},
         epicLength: 0,
         userStorieLength: 0,
         inventsLength: 0
@@ -191,13 +191,13 @@
           this.$refs.fileProject.value = null;
           document.getElementById("DavinciFile").click();
         }
-        
+
       },
 
       async backStep() {
-        if (this.step==1){
+        if (this.step == 1) {
           this.error = false;
-          this.projectOK = false;          
+          this.projectOK = false;
         }
       },
 
@@ -218,13 +218,13 @@
             this.alertError(
               "No ha seleccionado Archivo."
             );
-            this.error=true;
+            this.error = true;
           } else {
             if (Object.keys(this.invToRename).length > 0) {
               this.error = false;
               Object.keys(this.invToRename).forEach(key => {
                 if (Object.keys(this.invToRename).indexOf(this.invToRename[key]) > -1) {
-                  if (this.isDiffInvent(this.invToRename[key])){
+                  if (this.isDiffInvent(this.invToRename[key])) {
                     this.alertError(
                       `No renombraste ${this.invToRename[key]}`
                     );
@@ -252,25 +252,25 @@
                 }
               }
             } else {
-              this.error = false;              
+              this.error = false;
             }
           }
 
-          this.doContinue = !this.error;  
+          this.doContinue = !this.error;
 
           //End : Validaciones
           if (this.doContinue) {
             this.step = 1;
-            
+
           }
         }
-        if (this.step ==1){
+        if (this.step == 1) {
           this.importProject.project.Name = this.nameProject;
           this.epicLength = Object.keys(this.importProject.project.Epics.Types).length;
           this.userStorieLength = Object.keys(this.importProject.project.UserStories.Types).length;
           var count = 0;
 
-          this.importProject.project.Repository.forEach(repo=>{            
+          this.importProject.project.Repository.forEach(repo => {
             count = count + Object.keys(repo.WareHouse).length;
           })
 
@@ -279,16 +279,16 @@
                count = count + Object.keys(this.importProject.project.Repository[i].WareHouse).length;
           }
           */
-            this.inventsLength = count;
+          this.inventsLength = count;
         }
       },
       async finishStep() {
         var me = this;
-        
-        var dcode = await this.EncriptCode(this.importProject.project.Name);;
+
+        var dcode = await this.EncriptCode(this.nameProject);;
         console.log(`Davinci Code : ${dcode}`);
         this.importProject.project.Code = dcode;
-        this.importProject.project.Alias = this.importProject.project.Name.replace(/ /g,"");
+        this.importProject.project.Alias = this.nameProject.replace(/ /g, "");
         console.log(JSON.stringify(this.importProject));
         await this.axios
           .post("/api/project/import/", this.importProject).then(function (rs) {
@@ -340,9 +340,9 @@
           var me = this;
           readFile.onload = async function (event) {
             var content = event.target.result;
-            try{
+            try {
               var jsonImport = JSON.parse(content);
-            }catch{
+            } catch{
               me.alertError(`El Formato no es el correcto.`);
               return false;
             }
@@ -361,10 +361,10 @@
             //rs.data[] == json.inventions
             await me.axios.get("/api/invention/all/").then(rs => {
               var jsonInvents = rs.data;
-              
+
               for (var i = 0; i < jsonInvents.length; i++) {
                 for (var y = 0; y < jsonImport.inventions.length; y++) {
-                  if (jsonInvents[i].code == jsonImport.inventions[y].code && me.isDiffInv(jsonInvents[i],jsonImport.inventions[y]) ) {
+                  if (jsonInvents[i].code == jsonImport.inventions[y].code && me.isDiffInv(jsonInvents[i], jsonImport.inventions[y])) {
                     console.log(jsonInvents[i].name);
                     me.invToRename[jsonInvents[i].name] = jsonInvents[i].name;
                     me.error = true;
@@ -388,9 +388,6 @@
   };
 </script>
 <style scoped>
-
-
-
   .error {
     width: 100%;
     border: solid thin #fff;
